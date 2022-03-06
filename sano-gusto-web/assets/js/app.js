24720191bfc27bf927384a20aeacd08826bbe03c
diego_punto <diego@puntozip.com.mx>
2022-03-06 11:55:38 -0600

    Panle de administracion - pedidos recibidos

--- sano-gusto-web/assets/js/app.js
+++ sano-gusto-web/assets/js/app.js
@@ -25,4 +25,33 @@ function enviarPedido() {
           form.classList.add('was-validated')
         }, false)
       })
-  })()
+})()
+
+//DataTable
+$('.tabla').DataTable({
+    retrieve: true,
+    language: {
+        "sProcessing":     "Procesando...",
+        "sLengthMenu":     "Mostrando _MENU_ pedidos",
+        "sZeroRecords":    "No se encontraron resultados.",
+        "sEmptyTable":     "Ningún dato disponible en esta tabla.",
+        "sInfo":           "Mostrando pedidos del _START_ al _END_ de un total de _TOTAL_ pedidos.",
+        "sInfoEmpty":      "Mostrando pedidos del 0 al 0 de un total de 0 pedidos.",
+        "sInfoFiltered":   "(filtrado de un total de _MAX_ pedidos)",
+        "sInfoPostFix":    "",
+        "sSearch":         "Buscar:",
+        "sUrl":            "",
+        "sInfoThousands":  ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst":    "Primero",
+            "sLast":     "Último",
+            "sNext":     "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+    }
+});
